4c97a8daae9bea4a626e905fed67bd235c8b270e
Samuel Guerrero <samuelg510@gmail.com>
2023-11-06 12:05:05 -0500

    40. Parámetros en rutas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,4 +14,8 @@ class HomeController extends Controller
     {
         echo 'hola desde hola';
     }
+    public function parametros($id, $slug) //http://localhost/parametros/1/mi-slug?page=1
+    {
+        echo 'id=' . $id . ' | slug=' . $slug . ' | page=' . $_GET['page'];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'inicio'])->name('home.inicio');
 Route::get('/hola', [HomeController::class, 'hola'])->name('home.hola');
+Route::get('/parametros/{id}/{slug}', [HomeController::class, 'parametros'])->name('home.parametros');
