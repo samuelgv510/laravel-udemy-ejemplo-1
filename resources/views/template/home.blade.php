66d4e06e5b5d588dadd81dfc4fa72d065c2f2b6c
Samuel Guerrero <samuelg510@gmail.com>
2023-11-07 12:43:30 -0500

    49. Creación de template

---
+++ resources/views/template/home.blade.php
@@ -0,0 +1,4 @@
+@extends('../layouts.frontend')
+@section('content')
+    <h1>Título sólo desde la vista</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TemplateController;
 
 Route::get('/', [HomeController::class, 'inicio'])->name('home.inicio');
 Route::get('/hola', [HomeController::class, 'hola'])->name('home.hola');
 Route::get('/parametros/{id}/{slug}', [HomeController::class, 'parametros'])->name('home.parametros');
+
+Route::get('/template', [TemplateController::class, 'inicio'])->name('template.inicio');
