cba4f2f6c414c43fae52ec4046fbfb54c93f2dcf
Samuel Guerrero <samuelg510@gmail.com>
2023-11-10 15:21:25 -0500

    77. Migración con llaves foráneas

---
+++ database/migrations/2023_11_10_200250_create_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('slug')->unique();
+            $table->text('descripcion');
+            $table->date('fecha')->useCurrent();
+            $table->bigInteger('precio')->default('0');
+            $table->bigInteger('stock')->default('0');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
