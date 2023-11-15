b31f4c42023539ba307db0ef8a0d7928a3495cee
Samuel Guerrero <samuelg510@gmail.com>
2023-11-15 12:32:21 -0500

    109. Login con Auth::attempt y sesiones personalizadas

--- app/Http/Controllers/AccesoController.php
+++ app/Http/Controllers/AccesoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\UserMetadata;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AccesoController extends Controller
 {
@@ -16,6 +17,30 @@ class AccesoController extends Controller
     }
     public function login_post(Request $request)
     {
+        $request->validate(
+            [
+                'correo' => 'required|email:rfc,dns',
+                'password' => 'required|min:6'
+            ],
+            [
+                'correo.required' => 'El campo E-Mail está vacío',
+                'correo.email' => 'El E-Mail ingresado no es válido',
+                'password.required' => 'El campo Password está vacío',
+                'password.min' => 'El campo Password debe tener al menos 6 caracteres'
+            ]
+        );
+        if (Auth::attempt(['email' => $request->input('correo'), 'password' => $request->input('password')])) {
+            $usuario = UserMetadata::where('user_id', Auth::id())->first();
+
+            session(['user_metadata_id' => $usuario->id]);
+            session(['perfil_id' => $usuario->perfil_id]);
+            session(['perfil' => $usuario->perfil->nombre]);
+            return redirect()->intended('/');
+        } else {
+            session()->flash('css', 'danger');
+            session()->flash('mensaje', "Las credenciales indicadas no son válidas");
+            return redirect()->route('acceso.login');
+        }
     }
     public function registro()
     {
@@ -50,7 +75,7 @@ class AccesoController extends Controller
             [
                 'name' => $request->input('nombre'),
                 'email' => $request->input('correo'),
-                'password' => Hash::make($request->input('passsword')),
+                'password' => Hash::make($request->input('password')),
                 'created_at' => date('Y-m-d H:i:s')
             ]
         );
